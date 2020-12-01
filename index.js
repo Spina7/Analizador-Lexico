57bd826c8799647c60ce41b43852c72ba8a7501d
Spina <alonzopina7@gmail.com>
2020-11-30 21:17:17 -0600

    2.0

--- index.js
+++ index.js
@@ -8,35 +8,50 @@ function Analizar(){
 var expresion = document.getElementById('expresion');
 var resultado = document.getElementById('resultado');
 var array = expresion.value.split(/\s+/);
+var array2;
+
+var a=0;
 
 for (var i in array)
 {
-    if(/^[a-z]$/.test(array[i])){
-        resultado.innerHTML = resultado.innerHTML + array[i] + " es una variable\n";
+    ////////////////PALABRAS RESERVADAS////////////////
+    if(/(for|while|if|true|false|else|case|break)/.test(array[i])){
+        resultado.innerHTML = resultado.innerHTML + array[i] + " Palabra Reservada\n";
     }
     else if(/^[1-9]|[0-9]$/.test(array[i])){
         resultado.innerHTML = resultado.innerHTML + array[i] + " Numero\n";
     }
-    else if(/^(bin)[0-1]+$/.test(array[i])){
-        resultado.innerHTML = resultado.innerHTML + array[i] + " Binario\n";
-    }
-    else if(/^[*]$/.test(array[i])){
-        resultado.innerHTML = resultado.innerHTML + array[i] + " Multiplicacion\n";
-    }
-    else if(/^[+]$/.test(array[i])){
-        resultado.innerHTML = resultado.innerHTML + array[i] + " Suma\n";
-    }
-    else if(/^[-]$/.test(array[i])){
-        resultado.innerHTML = resultado.innerHTML + array[i] + " Menos\n";
-    }
-    else if(/^[/]$/.test(array[i])){
-        resultado.innerHTML = resultado.innerHTML + array[i] + " Division\n";
-    }
-    else if(/^[(]$/.test(array[i])){
-        resultado.innerHTML = resultado.innerHTML + array[i] + " Parentesis\n";
-    }
-    ////////////////PALABRAS RESERVADAS////////////////
-    else if(/(for|while|if|true|false|else|case|break)/.test(array[i])){
-        resultado.innerHTML = resultado.innerHTML + array[i] + " Palabra Reservada\n";
+    else{
+        array2 = array[i];
+        for (var h in array2){
+        if(/([A-Z])?[a-zA-Z]+/.test(array2[h])){
+            resultado.innerHTML = resultado.innerHTML + array2[h] + " Variable\n";
+        }
+        else if(/^[1-9]|[0-9]$/.test(array2[h])){
+            resultado.innerHTML = resultado.innerHTML + array2[h] + " Numero\n";
+        }
+        else if(/^(bin)[0-1]+$/.test(array2[h])){
+            resultado.innerHTML = resultado.innerHTML + array2[h] + " Binario\n";
+        }
+        else if(/^[*]$/.test(array2[h])){
+            resultado.innerHTML = resultado.innerHTML + array2[h] + " Multiplicacion\n";
+        }
+        else if(/^[+]$/.test(array2[h])){
+            resultado.innerHTML = resultado.innerHTML + array2[h] + " Suma\n";
+        }
+        else if(/^[-]$/.test(array2[h])){
+            resultado.innerHTML = resultado.innerHTML + array2[h] + " Menos\n";
+        }
+        else if(/^[/]$/.test(array2[h])){
+            resultado.innerHTML = resultado.innerHTML + array2[h] + " Division\n";
+        }
+        else if(/[(|)]/.test(array2[h])){
+            resultado.innerHTML = resultado.innerHTML + array2[h] + " Parentesis\n";
+        }
+        else if(/^[>]$/.test(array2[h])){
+            resultado.innerHTML = resultado.innerHTML + array2[h] + " Mayor\n";
+        }}
     }
 }}
+
+
